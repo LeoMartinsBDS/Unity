2e9bdbfb3e8fde4ed439719ebda2670af6a3201b
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-07-27 17:11:48 -0300

    Simple 3d platform game

---
+++ IntroducaoUnity/Assets/Scripts/altura_do_personagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class altura_do_personagem : MonoBehaviour
+{
+    public Transform personagem;
+    Text texto_da_interface;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        texto_da_interface = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(personagem.position.y);
+
+        texto_da_interface.text = Math.Round(personagem.position.y).ToString();
+    }
+}
